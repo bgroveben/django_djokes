02ae6947ca9497624a04315fb0cf16ef0719107b
bgroveben <bgroveben@gmail.com>
2022-11-04 12:58:08 -0700

    Create page for listing jokes

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,4 +1,14 @@
 from django.urls import path
 
+from .views import JokeListView
+
 app_name = 'jokes'
-urlpatterns = []
+urlpatterns = [
+    # Remember that only URL paths that begin with '/jokes/' will be handed
+    # off to the URLConf of the jokes app, so '' will actually be '/jokes/'.
+    path('', JokeListView.as_view(), name='list'),
+    # The second argument of the path() function must be a view function (as
+    # opposed to a class-based view), which is why you have to pass
+    # JokeListView.as_view(). The as_view() method of class-based views
+    # returns a view function.
+]
