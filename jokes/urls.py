7ebbb10f9d192c1a2687fd53ab1ad3112d8e784b
bgroveben <bgroveben@gmail.com>
2022-11-04 22:43:22 -0700

    Create DetailView

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 
-from .views import JokeListView
+from .views import JokeDetailView, JokeListView
 
 app_name = 'jokes'
 urlpatterns = [
-    # Remember that only URL paths that begin with '/jokes/' will be handed
-    # off to the URLConf of the jokes app, so '' will actually be '/jokes/'.
+    path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
     path('', JokeListView.as_view(), name='list'),
-    # The second argument of the path() function must be a view function (as
-    # opposed to a class-based view), which is why you have to pass
-    # JokeListView.as_view(). The as_view() method of class-based views
-    # returns a view function.
 ]
