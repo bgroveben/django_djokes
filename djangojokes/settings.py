adf55cb72e990ad8e69bcec43d805dcd30728ca2
bgroveben <bgroveben@gmail.com>
2022-11-10 10:57:17 -0800

    Add static files to Dajngo Jokes

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Added by me
+# Tells Django to look in the project root for a static folder.
+# It is a list, so you can set it to look in multiple places.
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
