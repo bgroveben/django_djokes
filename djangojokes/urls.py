be0abb195ce1045fa796083a3ea238bfe68e9dd4
bgroveben <bgroveben@gmail.com>
2022-11-20 12:06:09 -0800

    Update settings and urls to serve media files

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -1,9 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     # Admin
-    # admin/doc/ comes before admin/
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 
@@ -15,4 +16,4 @@ urlpatterns = [
     path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
