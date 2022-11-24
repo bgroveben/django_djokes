f2e3b4b4be0ceb8172af7c7608fee1a88551cf4b
bgroveben <bgroveben@gmail.com>
2022-11-24 10:49:31 -0800

    Update templates to allow for users votes, likes, and dislikes

--- jokes/urls.py
+++ jokes/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('joke/<slug>/update/', JokeUpdateView.as_view(), name='update'),
     path('joke/create', JokeCreateView.as_view(), name='create'),
     path('joke/<slug>/', JokeDetailView.as_view(), name='detail'),
+    path('joke/<slug>/vote/', vote, name='ajax-vote'),
     path('', JokeListView.as_view(), name='list'),
 ]
